1c36a0c40f03261361a9eb686fa5c00c91c40d90
Abraomukas <abraomukas@gmail.com>
2021-04-29 18:03:30 +0400

    stub service

--- java-stdin-stdout-ii/src/main/java/com/example/javastdinstdoutii/service/SolutionService.java
+++ java-stdin-stdout-ii/src/main/java/com/example/javastdinstdoutii/service/SolutionService.java
@@ -1,4 +1,32 @@
 package com.example.javastdinstdoutii.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Service
 public class SolutionService {
+
+    public List<String> generateSolution() {
+        List<String> result = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        int nextInt = scanner.nextInt();
+        double nextDouble = scanner.nextDouble();
+        String nextString = scanner.next();
+
+        result.add(String.valueOf(nextInt));
+        result.add(String.valueOf(nextDouble));
+        result.add(nextString);
+
+        return result;
+    }
+
+    public List<String> generateSolutionForDocker(int input_i, double input_d, String input_s) {
+        List<String> result = new ArrayList<>();
+
+        return result;
+    }
 }
