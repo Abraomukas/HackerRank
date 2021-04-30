1fe7578e7bab0952945c934dc860ccdcdb137dca
Abraomukas <abraomukas@gmail.com>
2021-04-30 12:21:03 +0400

    functionallity covered

--- java-stdin-stdout-ii/src/main/java/com/example/javastdinstdoutii/controller/SolutionController.java
+++ java-stdin-stdout-ii/src/main/java/com/example/javastdinstdoutii/controller/SolutionController.java
@@ -1,4 +1,32 @@
 package com.example.javastdinstdoutii.controller;
 
+import com.example.javastdinstdoutii.service.SolutionService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/solution")
 public class SolutionController {
+
+    private final SolutionService service;
+
+    @GetMapping(path = "/")
+    public List<String> generateSolution() {
+        return service.generateSolution();
+    }
+
+    @GetMapping(path = "/docker/{int}/{double}/{string}")
+    public List<String> generateSolutionForDocker(
+            @PathVariable("int") int input_i,
+            @PathVariable("double") double input_d,
+            @PathVariable("string") String input_s) {
+
+        return service.generateSolutionForDocker(input_i, input_d, input_s);
+    }
 }
--- java-stdin-stdout-ii/src/main/java/com/example/javastdinstdoutii/service/SolutionService.java
+++ java-stdin-stdout-ii/src/main/java/com/example/javastdinstdoutii/service/SolutionService.java
@@ -13,19 +13,31 @@ public class SolutionService {
         List<String> result = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Input I - integer");
         int nextInt = scanner.nextInt();
-        double nextDouble = scanner.nextDouble();
-        String nextString = scanner.next();
 
-        result.add(String.valueOf(nextInt));
-        result.add(String.valueOf(nextDouble));
-        result.add(nextString);
+        System.out.println("Input II - double");
+        double nextDouble = scanner.nextDouble();
+
+        System.out.println("Input III - String");
+        scanner.nextLine();
+        String nextString = scanner.nextLine();
+
+        scanner.close();
+
+        result.add("String: " + nextString);
+        result.add("Double: " + nextDouble);
+        result.add("Int: " + nextInt);
 
         return result;
     }
 
     public List<String> generateSolutionForDocker(int input_i, double input_d, String input_s) {
         List<String> result = new ArrayList<>();
+
+        result.add("String: " + input_s);
+        result.add("Double: " + input_d);
+        result.add("Int: " + input_i);
 
         return result;
     }
