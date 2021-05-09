457e2a6a622447a3f5325f9a621f4273d4a219b2
Abraomukas <abraomukas@gmail.com>
2021-05-09 17:45:57 +0400

    added generated project

---
+++ java-static-initializer-block/src/main/java/com/example/javastaticinitializerblock/JavaStaticInitializerBlockApplication.java
@@ -0,0 +1,13 @@
+package com.example.javastaticinitializerblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStaticInitializerBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStaticInitializerBlockApplication.class, args);
+	}
+
+}
---
+++ java-static-initializer-block/src/test/java/com/example/javastaticinitializerblock/JavaStaticInitializerBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javastaticinitializerblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStaticInitializerBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
