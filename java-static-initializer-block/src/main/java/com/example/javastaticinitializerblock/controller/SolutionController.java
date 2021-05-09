ede98ba66d3d2e276da994ba5b1cbfc4b911415b
Abraomukas <abraomukas@gmail.com>
2021-05-09 18:14:46 +0400

    added SolutionController.java

---
+++ java-static-initializer-block/src/main/java/com/example/javastaticinitializerblock/controller/SolutionController.java
@@ -0,0 +1,24 @@
+package com.example.javastaticinitializerblock.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/solution")
+public class SolutionController {
+
+    private final SolutionService service;
+
+    @GetMapping(path = "/")
+    public String generateSolution() {
+        return service.generateSolution();
+    }
+
+    @GetMapping(path = "/docker")
+    public String generateSolutionForDocker() {
+        return service.generateSolutionForDocker();
+    }
+}
