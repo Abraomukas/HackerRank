d9f7324e717289d783f79447205ddc624267e8cb
Abraomukas <abraomukas@gmail.com>
2021-05-10 08:38:48 +0400

    added project

---
+++ java-int-to-string/src/main/java/com/example/javainttostring/JavaIntToStringApplication.java
@@ -0,0 +1,13 @@
+package com.example.javainttostring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIntToStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIntToStringApplication.class, args);
+	}
+
+}
---
+++ java-int-to-string/src/test/java/com/example/javainttostring/JavaIntToStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javainttostring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIntToStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
