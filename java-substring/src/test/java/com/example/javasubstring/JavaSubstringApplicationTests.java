295c5e28dab5d10f6c1383e511b8d0f35ff2ce9d
Abraomukas <abraomukas@gmail.com>
2021-06-06 21:28:22 +0400

    added project + Dockerfile

---
+++ java-substring/src/test/java/com/example/javasubstring/JavaSubstringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javasubstring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSubstringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
