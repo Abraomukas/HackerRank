1acb5dc12f2ded18fc40ada9ba99a85ef7621d7e
Abraomukas <abraomukas@gmail.com>
2021-06-17 16:08:07 +0400

    stubs added to the service

---
+++ java-substring-comparisons/src/main/java/com/example/javasubstringcomparisons/service/SolutionService.java
@@ -0,0 +1,25 @@
+package com.example.javasubstringcomparisons.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SolutionService {
+
+    public List<String> generateSolution() {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+
+    public List<String> generateSolutionForDocker() {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+
+    public List<String> generateSolutionForTesting(String s, int k) {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
+}
