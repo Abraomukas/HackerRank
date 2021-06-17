04da6634d881fee76a570bce817d02c1a5b484a9
Abraomukas <abraomukas@gmail.com>
2021-06-17 16:04:57 +0400

    added controller

---
+++ java-substring-comparisons/src/main/java/com/example/javasubstringcomparisons/controller/SolutionController.java
@@ -0,0 +1,25 @@
+package com.example.javasubstringcomparisons.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/solution")
+public class SolutionController {
+
+    private final SolutionService service;
+
+    @RequestMapping(path = "/")
+    public List<String> generateSolution() {
+        return service.generateSolution();
+    }
+
+    @RequestMapping(path = "/docker")
+    public List<String> generateSolutionForDocker() {
+        return service.generateSolutionForDocker();
+    }
+}
