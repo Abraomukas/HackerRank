b9eb3336b7ca0410693b8c6b42a34597be39c930
Abraomukas <abraomukas@gmail.com>
2021-06-17 15:57:54 +0400

    added Makefile

---
+++ java-substring-comparisons/src/test/java/com/example/javasubstringcomparisons/JavaSubstringComparisonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javasubstringcomparisons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSubstringComparisonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
