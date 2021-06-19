1eab638f8b6536e606b02b646e082b72ed7b310c
Abraomukas <abraomukas@gmail.com>
2021-06-19 18:37:07 +0400

    added Maven project

---
+++ java-string-reverse/src/main/java/com/example/javastringreverse/JavaStringReverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javastringreverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStringReverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStringReverseApplication.class, args);
+	}
+
+}
