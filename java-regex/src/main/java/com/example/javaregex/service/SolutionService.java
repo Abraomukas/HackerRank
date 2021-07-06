7bde704f36cef13f1531d5029cf2ef37515e6790
Abraomukas <abraomukas@gmail.com>
2021-07-06 17:54:41 +0400

    added SolutionController and SolutionService

---
+++ java-regex/src/main/java/com/example/javaregex/service/SolutionService.java
@@ -0,0 +1,18 @@
+package com.example.javaregex.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SolutionService {
+
+    public List<String> generateSolution() {
+        return null;
+    }
+
+    public List<String> generateSolutionForDocker(Map<String, String> params) {
+        return null;
+    }
+}
