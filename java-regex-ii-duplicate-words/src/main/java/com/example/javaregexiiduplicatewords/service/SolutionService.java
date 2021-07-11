9d514492c8b8d94d263329d540242545eb9cc9ae
Abraomukas <abraomukas@gmail.com>
2021-07-11 16:08:10 +0400

    SolutionService mimics the code on HackerRank

--- java-regex-ii-duplicate-words/src/main/java/com/example/javaregexiiduplicatewords/service/SolutionService.java
+++ java-regex-ii-duplicate-words/src/main/java/com/example/javaregexiiduplicatewords/service/SolutionService.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class SolutionService {
@@ -13,12 +15,26 @@ public class SolutionService {
     public void generateSolution() {
         List<String> sentences = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        String regex = "\\b(\\w+)(\\b\\W+\\b\\1\\b)*";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
         int entries = scanner.nextInt();
 
         for (int i = 0; i < entries; i++) {
-            sentences.add(scanner.nextLine());
+            String input = scanner.nextLine();
+            Matcher matcher = pattern.matcher(input);
 
+            sentences.add("BEFORE - " + input);
+            while (matcher.find()) {
+                input = input.replaceAll(matcher.group(), matcher.group(1));
+            }
+            sentences.add("AFTER - " + input);
+        }
+
+        scanner.close();
+
+        for (String sentence : sentences) {
+            System.out.println(sentence);
         }
     }
 
